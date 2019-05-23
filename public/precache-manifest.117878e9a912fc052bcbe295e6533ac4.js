443dea82f3e9b64eaa4e18a5c972d696f5f5abce
武新飞 <xinfei.wu@qq.com>
2019-05-23 17:29:31 +0800

    add public, vendor

---
+++ public/precache-manifest.117878e9a912fc052bcbe295e6533ac4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80d496425554d0943b4a808ee41edf52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd51fdfa228975b8ab9a",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "1f93ad59924901b74ee6",
+    "url": "/static/js/2.843a569f.chunk.js"
+  },
+  {
+    "revision": "fd51fdfa228975b8ab9a",
+    "url": "/static/js/main.ccbbd0f0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
